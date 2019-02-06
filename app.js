6075525a1be50680539fe92fe8286fb50b648242
Jonathan <jtc7537@gmail.com>
2019-02-05 20:45:55 -0500

    ebay

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 // https://blog.syntonic.io/2017/07/07/reddit-bot-nodejs-example/
 // https://ssl.reddit.com/prefs/apps/
+// https://developer.ebay.com/DevZone/finding/CallRef/types/ItemFilterType.html
 
 require('dotenv').config();
 
 const Snoowrap = require('snoowrap');
 const Snoostorm = require('snoostorm');
+const request = require('request')
 
 const r = new Snoowrap({
     userAgent: 'reddit-bot-example-node',
@@ -14,4 +16,34 @@ const r = new Snoowrap({
     password: process.env.REDDIT_PASSWORD
 });
 
+console.log(r)
+
 const client = new Snoostorm(r);
+
+const streamOpts = {
+    subreddit: 'testingground4bots',
+    results: 25
+};
+
+const comments = client.CommentStream(streamOpts);
+
+comments.on('comment', comment => {
+    const {body} = comment;
+    console.log(body);
+    if(~body.search(/w\.amazon\.com\/.+/g)) {
+        var product;
+        const url = body.split('/')
+        url.forEach((e,i) => {
+            if(~e.search('amazon') && !product) product = url[i + 1].replace(/\-/g,' ')
+        })
+        request(`https://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsByKeywords&SERVICE-VERSION=1.0.0&SECURITY-APPNAME=${process.env.EBAY_APP_ID}&RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD&keywords=${product.replace(/\s/g,'%20')}&itemFilter(0).name=FreeShippingOnly&itemFilter(0).value=true`
+        , function(err, response, body){
+            console.log(JSON.parse(body).findItemsByKeywordsResponse[0].searchResult || err)
+            // console.log(JSON.parse(body).findItemsByKeywordsResponse[0].errorMessage[0].error)
+        })
+        comment.reply('test')
+
+    } else {
+        console.log(body)
+    }
+});
