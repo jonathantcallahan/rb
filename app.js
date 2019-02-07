ef1d0cf1d976041aa1a6567373143e5187bacd08
Jonathan <jtc7537@gmail.com>
2019-02-07 10:09:46 -0500

    comment

--- app.js
+++ app.js
@@ -27,10 +27,12 @@ const streamOpts = {
 
 const comments = client.CommentStream(streamOpts);
 
+let count = 0
+
 comments.on('comment', comment => {
     const {body} = comment;
     // console.log(body);
-    if(~body.search(/w\.amazon\.com\/.+/g)) {
+    if(body.indexOf('w.amazon.com/') > -1) {
         var product;
         const url = body.split('/')
         url.forEach((e,i) => {
@@ -42,11 +44,13 @@ comments.on('comment', comment => {
         //request(`https://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsByKeywords&SERVICE-VERSION=1.0.0&SECURITY-APPNAME=${process.env.EBAY_APP_ID}&RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD&keywords=${product.replace(/\s/g,'%20')}&itemFilter(0).name=FreeShippingOnly&itemFilter(0).value=true`)
         const response = '**^(It) ^(looks) ^(like) ^(this) ^(user) ^(has) ^(linked) ^(to) ^(a) ^(product) ^(on) ^(Amazon)** \n \n' + 
         affiliateMessage +
-        '##***Before you make a purchase consider, is this product...*** \n \n**1** a different version of something that you already own. If so, could you learn to be happy with the version you already have? \n \n**2** something that you could make, or reasonably learn to make? \n \n**3** something that you would use regularly for an extended period of time? If not, is it something that you could borrow from a friend or rent? \n \n**4** going to bring more happiness into your life? \n \n &nbsp; &nbsp; \n \n \n \nIf you have decided that you are going to purchase this product, have you checked eBay and Craigslist? \n \n &nbsp; &nbsp; \n \n \n \n^^^[<3](https://i.imgur.com/stC5T8C.jpg) ^^^I ^^^am ^^^a ^^^bot, ^^^if ^^^you ^^^have ^^^any ^^^feedback ^^^please ^^^send ^^^a ^^^message ^^^to ^^^/u/f_amazon_bot'
+        '##***Before you make a purchase consider, is this product...*** \n \n**1** a different version of something that you already own. If so, could you learn to be happy with the version you already have? \n \n**2** something that you could make, or reasonably learn to make? \n \n**3** something that you would use regularly for an extended period of time? If not, is it something that you could borrow from a friend or rent? \n \n**4** going to bring more happiness into your life? \n \n &nbsp; &nbsp; \n \n \n \nIf you have decided that you are going to purchase this product, have you checked eBay and Craigslist? \n \n &nbsp; &nbsp; \n \n \n \n^^^[<3](https://i.imgur.com/stC5T8C.jpg) ^^^I ^^^am ^^^a ^^^bot, ^^^if ^^^you ^^^have ^^^any ^^^feedback ^^^please ^^^send ^^^a ^^^message ^^^to ^^^[/u/f_amazon_bot](https://www.reddit.com/u/f_amazon_bot)'
         comment.reply(response)
-        console.log('is affiliate', isAffiliate)
+        count++
+        console.log('is affiliate', isAffiliate, count)
 
     } else {
-        // console.log(body)
+        count++
+        console.log('comment', count)
     }
 });
