4751028e33aa57722a66653c3e02dea5cc641fd3
Jonathan <Jonathan@iMac-3.local>
2019-02-06 23:40:46 -0500

    content git commit -m content

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 // https://ssl.reddit.com/prefs/apps/
 // https://developer.ebay.com/DevZone/finding/CallRef/types/ItemFilterType.html
 
-require('dotenv').config();
+require('dotenv').config(); 
 
 const Snoowrap = require('snoowrap');
 const Snoostorm = require('snoostorm');
@@ -16,12 +16,12 @@ const r = new Snoowrap({
     password: process.env.REDDIT_PASSWORD
 });
 
-console.log(r)
+// console.log(r)
 
 const client = new Snoostorm(r);
 
 const streamOpts = {
-    subreddit: 'testingground4bots',
+    subreddit: 'all',
     results: 25
 };
 
@@ -29,18 +29,24 @@ const comments = client.CommentStream(streamOpts);
 
 comments.on('comment', comment => {
     const {body} = comment;
-    console.log(body);
+    // console.log(body);
     if(~body.search(/w\.amazon\.com\/.+/g)) {
         var product;
         const url = body.split('/')
         url.forEach((e,i) => {
             if(~e.search('amazon') && !product) product = url[i + 1].replace(/\-/g,' ')
         })
+
+        const isAffiliate = !!~url.join('').search('tag=')
+        const affiliateMessage = isAffiliate ? '`!THE LINK IS AN AFFILIATE LINK. THE USER WHO POSTED IT HAS A VESTED INTEREST IN YOU PURCHASING IT!`[what is an affiliate link](https://yourconsumerguide.co.uk/2018/01/07/spot-affiliate-marketing/)\n \n' : '`The link is not an affiliate link` [?](https://yourconsumerguide.co.uk/2018/01/07/spot-affiliate-marketing/)\n \n'
         //request(`https://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsByKeywords&SERVICE-VERSION=1.0.0&SECURITY-APPNAME=${process.env.EBAY_APP_ID}&RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD&keywords=${product.replace(/\s/g,'%20')}&itemFilter(0).name=FreeShippingOnly&itemFilter(0).value=true`)
-        const response = '**It looks like this user has linked to a product on Amazon** \n \n *Before you make a purchase consider, is this product...* \n \n * a different version of something that you already own, and could you be content with the version you already have? \n \n * something that you could make, or reasonable learn to make? \n \n * something that you would use regularly for an extended period of time? If not, is it something that you could borrow or rent? \n \n * going to bring more happiness into your life? \n \n &nbsp; &nbsp; \n \n If do decide you need to purchase this product, have you checked eBay and Craigslist?'
+        const response = '**^It ^looks ^like ^this ^user ^has ^linked ^to ^a ^product ^on ^Amazon** \n \n' + 
+        affiliateMessage +
+        '##***Before you make a purchase consider, is this product...*** \n \n**1** a different version of something that you already own. If so, could you learn to be happy with the version you already have? \n \n**2** something that you could make, or reasonably learn to make? \n \n**3** something that you would use regularly for an extended period of time? If not, is it something that you could borrow from a friend or rent? \n \n**4** going to bring more happiness into your life? \n \n &nbsp; &nbsp; \n \n \n \nIf you have decided that you are going to purchase this product, have you checked eBay and Craigslist? \n \n &nbsp; &nbsp; \n \n \n \n^^^[<3](https://i.imgur.com/stC5T8C.jpg) ^^^I ^^^am ^^^a ^^^bot, ^^^if ^^^you ^^^have ^^^any ^^^feedback ^^^please ^^^send ^^^a ^^^message ^^^to ^^^/u/f_amazon_bot'
         comment.reply(response)
+        console.log('is affiliate', isAffiliate)
 
     } else {
-        console.log(body)
+        // console.log(body)
     }
 });
