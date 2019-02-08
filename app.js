1116107f81ccd0d17c2870a6604d3b664fec2500
Jonathan <jtc7537@gmail.com>
2019-02-07 19:04:08 -0500

    content

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const r = new Snoowrap({
 const client = new Snoostorm(r);
 
 const streamOpts = {
-    subreddit: 'testingground4bots',
+    subreddit: 'all',
     results: 25
 };
 
@@ -29,7 +29,7 @@ const comments = client.CommentStream(streamOpts);
 
 let count = 0
 
-const tinyText = () => {}
+const tinyText = sentence => sentence.split(' ').map(e => `^${e}`).join(' ')
 
 comments.on('comment', comment => {
     const {body} = comment;
@@ -41,19 +41,31 @@ comments.on('comment', comment => {
             if(~e.search('amazon') && !product) product = url[i + 1].replace(/\-/g,' ')
         })
 
-
+        const body1 = 'Before clicking consider: will this purchase bring you happiness? If so, have you considered buying locally or used from another person?'
+        const body2 = 'Will this purchase bring you happiness? If so, have you considered buying locally or used from another person?'
         const isAffiliate = !!~url.join('').search('tag=')
-        const affiliateMessage = isAffiliate ? '`!THE LINK IS AN AFFILIATE LINK. THE USER WHO POSTED IT HAS A VESTED INTEREST IN YOU PURCHASING IT!`[what is an affiliate link](https://yourconsumerguide.co.uk/2018/01/07/spot-affiliate-marketing/)\n \n' : '`The link is not an affiliate link` [?](https://yourconsumerguide.co.uk/2018/01/07/spot-affiliate-marketing/)\n \n'
+        const affiliateMessage = isAffiliate ? '`!THE LINK IS AN AFFILIATE LINK!` \n \n `THE USER WHO POSTED IT HAS A VESTED INTEREST IN YOU PURCHASING IT`  [what is an affiliate link](https://yourconsumerguide.co.uk/2018/01/07/spot-affiliate-marketing/)\n \n' : 
+                                                '`The link is not an affiliate link` [[?]](https://yourconsumerguide.co.uk/2018/01/07/spot-affiliate-marketing/)\n \n'
         //request(`https://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsByKeywords&SERVICE-VERSION=1.0.0&SECURITY-APPNAME=${process.env.EBAY_APP_ID}&RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD&keywords=${product.replace(/\s/g,'%20')}&itemFilter(0).name=FreeShippingOnly&itemFilter(0).value=true`)
         const response = '**^(It) ^(looks) ^(like) ^(this) ^(user) ^(has) ^(linked) ^(to) ^(a) ^(product) ^(on) ^(Amazon)** \n \n' + 
         affiliateMessage +
-        '###**Before you make a purchase consider, is this product...** \n \n**1** a different version of something that you already own. If so, could you learn to be happy with the version you already have? \n \n**2** something that you could make, or reasonably learn to make? \n \n**3** something that you would use regularly for an extended period of time? If not, is it something that you could borrow from a friend or rent? \n \n**4** going to bring more happiness into your life? \n \n &nbsp; &nbsp; \n \n \n \nIf you have decided that you are going to purchase this product, have you checked eBay and Craigslist? \n \n &nbsp; &nbsp; \n \n \n \n^^^[<3](https://i.imgur.com/stC5T8C.jpg) ^^^I ^^^am ^^^a ^^^bot, ^^^if ^^^you ^^^have ^^^any ^^^feedback ^^^please ^^^send ^^^a ^^^message ^^^to ^^^[/u/f_amazon_bot](https://www.reddit.com/u/f_amazon_bot)'
+        tinyText(isAffiliate ? body2 : body1) +
+        '&nbsp; &nbsp; \n \n \n \n^^^[<3](https://i.imgur.com/stC5T8C.jpg) ^^^I ^^^am ^^^a ^^^bot, ^^^if ^^^you ^^^have ^^^any ^^^feedback ^^^please ^^^send ^^^a ^^^message ^^^to ^^^[/u/f_amazon_bot](https://www.reddit.com/u/f_amazon_bot)'
+        // '###**Before you make a purchase consider, is this product...** \n \n' +
+        // tinyText('**1** a different version of something that you already own. If so, could you learn to be happy with the version you already have?') + 
+        // '\n \n' +
+        // tinyText('**2** something that you could make, or reasonably learn to make?') + 
+        // '\n \n' +  
+        // tinyText('**3** something that you would use regularly for an extended period of time? If not, is it something that you could borrow from a friend or rent?') + 
+        // '\n \n' +  
+        // tinyText('**4** going to bring more happiness into your life?') +
+        // '\n \n &nbsp; &nbsp; \n \n \n \nIf you have decided that you are going to purchase this product, have you checked eBay and Craigslist? \n \n &nbsp; &nbsp; \n \n \n \n^^^[<3](https://i.imgur.com/stC5T8C.jpg) ^^^I ^^^am ^^^a ^^^bot, ^^^if ^^^you ^^^have ^^^any ^^^feedback ^^^please ^^^send ^^^a ^^^message ^^^to ^^^[/u/f_amazon_bot](https://www.reddit.com/u/f_amazon_bot)'
         comment.reply(response)
         count++
         console.log('is affiliate', isAffiliate, count)
 
     } else {
-        count++
-        console.log('comment', count)
+        // count++
+        // console.log('comment', count)
     }
 });
